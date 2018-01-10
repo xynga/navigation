635e82a3b6a6d29930c040519e6998dc6c034f46
alaroche <(Blue22810)>
2018-01-10 12:38:00 -0500

    Attempting to merge via github rather than locally on intellij

--- testing/src/app/tab-components/TabOne.ts
+++
@@ -1,9 +0,0 @@
-import {Component} from '@angular/core';
-
-@Component({
-  template: `<h1>Tab 1</h1>`
-})
-
-export class TabOneComponent {
-
-}
--- testing/src/app/tab-components/TabTwo.ts
+++
@@ -1,9 +0,0 @@
-import {Component} from '@angular/core';
-
-@Component({
-  template: `<h1>Tab 2</h1>`
-})
-
-export class TabTwoComponent {
-
-}
