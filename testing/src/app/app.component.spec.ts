d80e22427c3eb8a803dfaef758367d3caba2be4c
jaronson <aronson_jesse@wheatoncollege.edu>
2018-01-17 09:49:42 -0500

    start testing menu component

--- testing/src/app/app.component.spec.ts
+++ testing/src/app/app.component.spec.ts
@@ -1,27 +1,44 @@
 import { TestBed, async } from '@angular/core/testing';
-import { AppComponent } from './app.component';
-describe('AppComponent', () => {
+import { MenuComponent, TabListComponent } from 'xynga-navigation';
+describe('Menu-Component', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        MenuComponent
       ],
     }).compileComponents();
   }));
-  it('should create the app', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
-  }));
-  it(`should have as title 'app'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
-  }));
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
+  it('should have menu closed when "host focus" event is triggered', async(() => {
+    const fixture = TestBed.createComponent(MenuComponent);
+    const menu = fixture.debugElement.componentInstance;
+    menu.focusActive = false;
+    menu.onHostFocus();
     fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
+    expect(menu.Open).not.toBeTruthy();
+  }));
+  it('should have menu open after click event is triggered', async(() => {
+    const fixture = TestBed.createComponent(MenuComponent);
+    const menu = fixture.debugElement.componentInstance;
+    menu.clickToggle = true;
+    menu.onHostClick();
+    fixture.detectChanges();
+    expect(menu.Open).toBeTruthy();
+  }));
+  it('should have menu closed after "mouse over" event is triggered', async(() => {
+    const fixture = TestBed.createComponent(MenuComponent);
+    const menu = fixture.debugElement.componentInstance;
+    menu.mouseActivate = false;
+    menu.onHostMouseOver();
+    fixture.detectChanges();
+    expect(menu.Open).not.toBeTruthy();
+  }));
+  it('should have menu open after "mouse out" event is triggered', async(() => {
+    const fixture = TestBed.createComponent(MenuComponent);
+    const menu = fixture.debugElement.componentInstance;
+    menu.Open = true;
+    menu.mouseActivate = false;
+    menu.onHostMouseOut();
+    fixture.detectChanges();
+    expect(menu.Open).toBeTruthy();
   }));
 });
